eee6507f55d26c9642f6689f43d718142f897b3e
abouslah <ab.bouslah93@gmail.com>
2022-01-03 22:14:06 +0100

    implement getProducts service

---
+++ src/app/product-list/product-list.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import {Product} from '../product';
+import { ProductService } from 'app/product.service';
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent implements OnInit {
+  products: Product[];
+  constructor(private _productService :ProductService) { };
+
+  ngOnInit() {
+  }
+
+}
